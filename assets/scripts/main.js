5717923ef0ad8603244b4dabfe9b5d5bdb46581d
anniebombanie <anniebombanie@gmail.com>
2019-07-11 09:27:25 -0400

    intial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+/*
+- main screen: instructions and EAT (START) button
+- on click of start button, count down timer starts
+- sushi appears & disappears randomly on plates
+- when you click on a sushi, scoreboard updates with points
+- if no sushi is clicked, score doesn't change
+- when timer runs out, display custom message depending on how many points you get
+- eat more sushi (PLAY AGAIN) button resets game
+*/
